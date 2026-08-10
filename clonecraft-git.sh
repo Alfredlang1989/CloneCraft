46b096f9463dac0d76142654bdc23664e66587d7
alang <alfred.lang@lasrv.de>
2026-08-11 00:30:04 +0200

    Prepare CloneCraft GitHub bootstrap

--- clonecraft-git.sh
+++ clonecraft-git.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-REPO_URL="${CLONECRAFT_REPO_URL:-https://github.com/Alfredlang1989/CloneCraft.git}"
+REPO_URL="${CLONECRAFT_REPO_URL:-git@github.com:Alfredlang1989/CloneCraft.git}"
 BRANCH="${CLONECRAFT_BRANCH:-main}"
 CMD="${1:-help}"
 
