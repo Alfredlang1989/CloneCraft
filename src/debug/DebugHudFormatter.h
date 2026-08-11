d1c8b14174878dce52af9608139cc8fad6868f67
Alfredlang1989 <56228352+Alfredlang1989@users.noreply.github.com>
2026-08-11 23:20:35 +0200

    Pack debug HUD snapshot fields

--- src/debug/DebugHudFormatter.h
+++ src/debug/DebugHudFormatter.h
@@ -10,11 +10,7 @@ namespace debug
     struct BiomeWeightLine { std::string id; std::string displayName; double weight = 0.0; };
     struct DebugHudSnapshot
     {
-        float latestFps=0.0f,averageFps=0.0f,latestFrameMs=0.0f,averageFrameMs=0.0f;
-        float renderLocalX=0.0f,renderLocalY=0.0f,renderLocalZ=0.0f;
-        float dynamicLocalX=0.0f,dynamicLocalY=0.0f,dynamicLocalZ=0.0f;
         std::int64_t dynamicEdgeBlocks=0;
-        float fractionX=0.0f,fractionY=0.0f,fractionZ=0.0f;
         std::int64_t sectorX=0,sectorY=0,sectorZ=0;
         std::int64_t regionX=0,regionY=0,regionZ=0;
         std::int64_t sectionX=0,sectionY=0,sectionZ=0;
@@ -26,19 +22,27 @@ namespace debug
         std::int64_t renderSectionX=0,renderSectionY=0,renderSectionZ=0;
         std::int64_t renderGroupX=0,renderGroupY=0,renderGroupZ=0;
         std::int64_t groupEdgeBlocks=0;
-        std::vector<BiomeWeightLine> biomes;
-        bool voxelLoaded=false; std::string voxelId; std::uint16_t voxelRuntimeId=0;
-        bool targetPresent=false; std::string targetId,targetDisplayName,targetTexture; std::vector<std::string> targetTags;
-        std::uint16_t targetRuntimeId=0; double targetDistance=0.0; bool targetSolid=false,targetOpaque=false,targetTransparent=false;
+        double targetDistance=0.0;
         std::int64_t targetSectorX=0,targetSectorY=0,targetSectorZ=0;
         std::int64_t targetRegionX=0,targetRegionY=0,targetRegionZ=0;
         std::int64_t targetSectionX=0,targetSectionY=0,targetSectionZ=0;
         std::int64_t targetGroupX=0,targetGroupY=0,targetGroupZ=0;
         std::int64_t targetChunkX=0,targetChunkY=0,targetChunkZ=0;
         std::int64_t targetBlockX=0,targetBlockY=0,targetBlockZ=0;
-        std::size_t loadedChunks=0,loadedGroups=0; std::int64_t streamingRadius=0;
+        std::size_t loadedChunks=0,loadedGroups=0;
+        std::int64_t streamingRadius=0;
         std::size_t generatedChunks=0,evictedChunks=0,queuedChunks=0,readyChunks=0;
-        double yawDegrees=0.0,pitchDegrees=0.0; bool flashlightEnabled=false;
+        double yawDegrees=0.0,pitchDegrees=0.0;
+        std::vector<BiomeWeightLine> biomes;
+        std::vector<std::string> targetTags;
+        std::string voxelId,targetId,targetDisplayName,targetTexture;
+        float latestFps=0.0f,averageFps=0.0f,latestFrameMs=0.0f,averageFrameMs=0.0f;
+        float renderLocalX=0.0f,renderLocalY=0.0f,renderLocalZ=0.0f;
+        float dynamicLocalX=0.0f,dynamicLocalY=0.0f,dynamicLocalZ=0.0f;
+        float fractionX=0.0f,fractionY=0.0f,fractionZ=0.0f;
+        std::uint16_t voxelRuntimeId=0,targetRuntimeId=0;
+        bool voxelLoaded=false,targetPresent=false,targetSolid=false,targetOpaque=false,
+             targetTransparent=false,flashlightEnabled=false;
     };
     std::string formatDebugHud( const DebugHudSnapshot &snapshot );
 } // namespace debug
